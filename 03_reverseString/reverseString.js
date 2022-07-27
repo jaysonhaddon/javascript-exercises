7f6f214b0bf6db4f544a1a9448a28f83247541db
Jayson <jayson.haddon@outlook.com>
2022-07-26 20:31:13 -0600

    Completed excercises 01-04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let arr = string.split("");
+    let length = arr.length;
+    result = "";
 
+    for (let i = length - 1; i >= 0; i--) {
+        result += arr[i];
+    }
+
+    return result;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+    let removeArr = Array.from(arguments);
+    let mainArr = removeArr.shift();
+    for (let arg of removeArr) {
+        if (mainArr.includes(arg)) {
+            let index = mainArr.findIndex(element => element === arg);
+            mainArr.splice(index, 1);    
+        }
+    }
+    return mainArr;
 };
 
 // Do not edit below this line
