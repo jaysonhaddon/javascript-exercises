6db2c1b38f2507c15bbb979cca3d36fcc2cd9500
Jayson <jayson.haddon@outlook.com>
2022-07-27 20:09:53 -0600

    Completed sections 5-7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  let result = ((temp - 32) * (5 / 9));
 
+  if (result % 1 !== 0) {
+    return Number(result.toFixed(1));
+  } else {
+    return result;
+  }
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+  let result = (temp * (9 / 5) + 32);
 
+  if (result % 1 !== 0) {
+    return Number(result.toFixed(1));
+  } else {
+    return result;
+  }
 };
 
 // Do not edit below this line
